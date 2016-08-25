3648dfbdf94c3b2af10e328caeb20bfccc60d396
Andy Wang <wangyaodi@gmail.com>
2016-08-25 15:18:20 +0800

    allow to verify url

--- ext/wrap_c_example/wrap_c_example.c
+++ ext/wrap_c_example/wrap_c_example.c
@@ -22,6 +22,8 @@ extern int gen_default_key(const char* uri,
   int url_valid_time,
   char*  res_key);
 
+extern int verify_url(const char* url, const char* key);
+
 static VALUE
 greeting_init(VALUE self, VALUE name, VALUE key, VALUE time) {
   rb_iv_set(self, "@uri", name);
@@ -37,6 +39,11 @@ static VALUE genkey(VALUE self) {
   return rb_str_new2(out);
 }
 
+static VALUE verify(VALUE self) {
+  int i = verify_url(RSTRING_PTR(rb_iv_get(self, "@uri")), RSTRING_PTR(rb_iv_get(self, "@key")));
+  return i == 0 ? Qtrue : Qfalse;
+}
+
 void
 Init_wrap_c_example_c() {
   rb_mWrapCExample = rb_define_module("WrapCExample");
@@ -45,4 +52,5 @@ Init_wrap_c_example_c() {
   rb_define_method(rb_cGreeting, "initialize", greeting_init, 3);
   rb_define_method(rb_cGreeting, "hello", greeting_hello, 0);
   rb_define_method(rb_cGreeting, "genkey", genkey, 0);
+  rb_define_method(rb_cGreeting, "verify", verify, 0);
 }
